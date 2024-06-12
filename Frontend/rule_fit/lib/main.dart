f2f50b5797e8245a5b6eb50bd43073e6a8c51b64
tnptnw <thanapat.than@kmutt.ac.th>
2024-06-12 17:53:49 +0700

    profile page

--- Frontend/rule_fit/lib/main.dart
+++ Frontend/rule_fit/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rule_fit/pages/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ProfilePage(),
     );
   }
 }
